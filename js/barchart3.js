c5f85d3462a45afb3e89fc620f09b8f1c4766727
Miguel Mendes <miguel.mendes@intelie.com.br>
2015-07-03 13:54:09 -0300

    horizontal bar chart version 2.2 with svg - ordinal

--- js/barchart3.js
+++ js/barchart3.js
@@ -5,6 +5,9 @@
 var width = 960,
     height = 500;
 
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
 var y = d3.scale.linear()
     .range([height, 0]);
 
@@ -13,22 +16,21 @@ var chart = d3.select(".chart")
     .attr("height", height);
 
 d3.tsv("data.tsv", type, function(error, data) {
+    x.domain(data.map(function(d) { return d.name; }));
     y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-    var barWidth = width / data.length;
 
     var bar = chart.selectAll("g")
         .data(data)
         .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+        .attr("transform", function(d) { return "translate(" + x(d.name) + ",0)"; });
 
     bar.append("rect")
         .attr("y", function(d) { return y(d.value); })
         .attr("height", function(d) { return height - y(d.value); })
-        .attr("width", barWidth - 1);
+        .attr("width", x.rangeBand());
 
     bar.append("text")
-        .attr("x", barWidth / 2)
+        .attr("x", x.rangeBand() / 2)
         .attr("y", function(d) { return y(d.value) + 3; })
         .attr("dy", ".75em")
         .text(function(d) { return d.value; });
